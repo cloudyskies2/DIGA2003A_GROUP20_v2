6b835bbd78afcbafe22ffd60127039ea54421584
ceow4s <2859619@students.wits.ac.za>
2025-06-16 14:46:18 +0200

    Health system

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,28 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int health;
+    public int maxHealth = 10;
+   
+    public SpriteRenderer playerSr;
+    public playerController1 playerMovement;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int amount)
     {
-        
+        health -= amount; 
+        if(health <= 0)
+        {
+            playerSr.enabled = false;
+            playerMovement.enabled = false;
+        }
     }
 }
